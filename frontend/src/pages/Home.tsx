f9cf2f5eaeb877a856d3009b7dc8aee6bad6ef9a
vctrubio <vctrubio@gmail.com>
2025-03-13 18:40:12 +0100

    account integration

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,9 @@
+import Account from '../components/boxify/Account';
+
 interface Item {
   label: string;
   path: string;
+  component: React.ComponentType;
   theme?: {
     dark: string;
     light: string;
@@ -11,6 +14,7 @@ const items: Item[] = [
   { 
     label: 'Account', 
     path: '/',
+    component: Account,
     theme: {
       dark: 'bg-blue-900',
       light: 'bg-blue-200'
@@ -19,6 +23,7 @@ const items: Item[] = [
   { 
     label: 'Contracts', 
     path: '/',
+    component: () => <>Contract Information</>,
     theme: {
       dark: 'bg-purple-900',
       light: 'bg-purple-200'
@@ -32,10 +37,14 @@ function Boxify({ items }: { items: Item[] }): JSX.Element {
     // Get theme colors or use default colors if theme is not defined
     const darkBg = i.theme?.dark || 'bg-gray-800';
     const lightBg = i.theme?.light || 'bg-gray-100';
+    const Component = i.component;
     
     return (
       <div className={`relative border p-4 rounded m-8 min-h-20`}>
-      <div className={`absolute left-2 top-0 -mt-5 z-10 p-2 border rounded cursor-pointer ${lightBg} dark:${darkBg} dark:text-blue-500 text-orange-500`}>{i.label}</div>
+        <div className={`absolute left-2 top-0 -mt-5 z-10 p-2 border rounded cursor-pointer ${lightBg} dark:${darkBg} dark:text-blue-500 text-orange-500`}>{i.label}</div>
+        <div className="pt-4">
+          <Component />
+        </div>
       </div>
     )
   }
