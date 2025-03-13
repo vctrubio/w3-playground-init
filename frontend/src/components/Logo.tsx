8a42b7a6d8a33e1d0b73406ad5524d78bd51c8f9
vctrubio <vctrubio@gmail.com>
2025-03-13 16:33:37 +0100

    wit icon on tabs

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <Link to="/" className="flex items-center space-x-2">
       <div className="w-8 h-8 dark:bg-blue-600 rounded-md flex items-center justify-center bg-orange-500">
-        <span className="text-white font-bold">W3</span>
+        <span className="text-white font-bold text-xs">w3</span>
       </div>
     </Link>
   );
