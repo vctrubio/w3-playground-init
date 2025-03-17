6f14be88c207cab13f646b3aed46014167298f44
vctrubio <vctrubio@gmail.com>
2025-03-17 13:29:57 +0100

    remix upload logs

--- hardhat/contracts/Erc1155.sol
+++ hardhat/contracts/Erc1155.sol
@@ -2,6 +2,16 @@
 pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
+/*
+deployed firt on remix logs::
+https://sepolia.etherscan.io/tx/0x37662d27d852c0de544aa3b945b67ea87c48860c96358cf37871f54e12758577
+tx: 0x37662d27d852c0de544aa3b945b67ea87c48860c96358cf37871f54e12758577
+from: 0x34b68353F8753F7e019987168b1Eacf0F76AE5c9
+to: 0x7a32108df38cc184fc7bfca7b477e7767d19590d
+
+https://testnets.opensea.io/collection/unidentified-contract-c4b7a0b5-1859-4449-8b4e-ed33
+*/
+
 contract HouseUrban is ERC1155 {
     string public tokenName = "HouseUrban";
     string public symbol = "HHU";
--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,4 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
+// Add dotenv to manage environment variables
+require("dotenv").config();
+
+// Get private key from environment variables
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -15,12 +22,20 @@ module.exports = {
         initialIndex: 0,
         count: 10
       }
-    }
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL || "https://rpc.sepolia.org",
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      chainId: 11155111,
+    },
   },
   paths: {
     sources: "./contracts",
     tests: "./test",
     cache: "./cache",
     artifacts: "./artifacts"
-  }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
